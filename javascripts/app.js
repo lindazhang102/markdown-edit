24f9a41454d081e57bb2339a51e066add3f5dbd8
lindazhang102 <lindazhang102@gmail.com>
2014-12-10 09:23:03 +0800

    add button1 and button2 in app.js

--- javascripts/app.js
+++ javascripts/app.js
@@ -4,3 +4,36 @@ $(document).ready(function(){
     $('#html').html(text);
   });
 });
+
+$(document).ready(function(){
+  $('#button1').click(function(){
+     var selection=window.getSelection()
+     text=$("#markdown").replaceSelectedText("'**'+selection'**'");
+     $('#markdown').val(text);
+     console.log(text);
+     text = markdown.toHTML( text );
+     $('#html').html(text);
+    });
+});
+
+$(document).ready(function(){
+  var selection;
+  $('textarea').keyup(function(){
+    console.log(selection)
+    selection=window.getSelection().toString()
+  });
+
+  $('textarea').mouseup(function(){
+    console.log(selection)
+    selection=window.getSelection().toString()
+  });
+
+  $('#button2').click(function(){
+    text = $('#markdown').val().replace(selection,'*'+selection+'*');
+    console.log(text);
+    $('#markdown').val(text);
+    console.log(text);
+    text = markdown.toHTML( text );
+    $('#html').html(text);
+  });
+});
